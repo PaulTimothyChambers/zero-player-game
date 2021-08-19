4d0a1c8ea586b29b902fd353d5f8d1f3db52e997
Paul Chambers <paultimothychambers@gmail.com>
2021-08-18 23:45:14 -0600

    Create class constructor

---
+++ Square.js
@@ -0,0 +1,7 @@
+class Square {
+  constructor(position, gNum) {
+    this.position = position;
+    this.value = 0;
+    this.groupNumber = gNum; //the id of the group surrounding this instance
+  }
+}
